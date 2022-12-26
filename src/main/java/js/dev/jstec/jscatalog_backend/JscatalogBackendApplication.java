d2f8e8d6de65228a7d620352e231bddb5ff2aaaa
JS-Nascimento <jorge@jstec.dev.br>
2022-12-26 13:16:09 -0300

    initial commit

---
+++ src/main/java/js/dev/jstec/jscatalog_backend/JscatalogBackendApplication.java
@@ -0,0 +1,13 @@
+package js.dev.jstec.jscatalog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscatalogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscatalogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/js/dev/jstec/jscatalog_backend/JscatalogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package js.dev.jstec.jscatalog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscatalogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
