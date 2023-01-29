53ad74b618c92ecdd7ad99ff8369b3b4007c209b
Jorge Nascimento <jorge@jstec.dev.br>
2023-01-29 15:15:50 -0300

    Continuing the implementation of integration tests.

--- src/test/java/js/dev/jstec/jscatalog_backend/rest/ProductControllerIT.java
+++ src/test/java/js/dev/jstec/jscatalog_backend/rest/ProductControllerIT.java
@@ -1,25 +1,18 @@
 package js.dev.jstec.jscatalog_backend.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import js.dev.jstec.jscatalog_backend.Factory;
-import js.dev.jstec.jscatalog_backend.domain.entities.Category;
-import js.dev.jstec.jscatalog_backend.domain.entities.Product;
 import js.dev.jstec.jscatalog_backend.rest.DTOS.ProductDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.util.List;
-
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,10 +27,9 @@ public class ProductControllerIT {
 
     private Integer existingId;
     private Integer nonExistingId;
-    private Integer dependentId;
-    private PageImpl page;
-    private Product product;
-    private Category category;
+
+
+
     private ProductDTO productDTO;
 
     private ObjectMapper objectMapper;
@@ -46,21 +38,20 @@ public class ProductControllerIT {
     void setUp(){
         existingId = 1;
         nonExistingId = 1000;
-        dependentId = 4;
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
 
-        product = Factory.createProduct();
-        category = Factory.createCategory();
+
+
         productDTO = Factory.createProductDTO();
-        page = new PageImpl<>( List.of(product));
+
 
     }
 
     @Test
     public void findAllShouldReturnSortedPageWhenGetSortedPage() throws Exception{
 
-        ResultActions result =
+
                 mockMvc.perform( MockMvcRequestBuilders.get("/api/products?page=0&size=12&sort=name,asc")
                         .accept( MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk())
